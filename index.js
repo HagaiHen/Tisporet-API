475626200d65b5f3bd170453c37e136c10db6b66
najeeb abdalla <92922407+najeebWorld@users.noreply.github.com>
2022-12-28 17:16:58 +0200

    added customer operations

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const cors = require('cors');
 const bodyParser =require('body-parser')
 const config = require('./config');
 const barberRoutes = require('./routes/barber-routes');
+const customerRoutes = require('./routes/customer-routes');
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
-app.use('/api', barberRoutes.routes);
+app.use('/api', barberRoutes.routes,customerRoutes.routes);
 
-app.listen(config.port, ()=> console.log('App is listening on url http://localhost:' + config.port));
+
+app.listen(config.port, ()=> console.log('App is listening on url http://' + config.host + ':' + config.port));
