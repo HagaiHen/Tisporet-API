52af76d0fa80fbe6b54117cbf83b5099dec46e69
bar.goldenberg <bar.goldenberg@similarweb.com>
2023-01-08 17:26:19 +0200

    done with email sending

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,6 +1,8 @@
-'use strict';
-const {db} = require('../db')
-var moment = require('moment') // JS time module (run npm install moment in this project in order to make it work). 
+"use strict";
+const { db } = require("../db");
+var moment = require("moment"); // JS time module (run npm install moment in this project in order to make it work).
+const { sendMailToWaitlistCustomer } = require("./mailController");
+const { getCustomerFromDB } = require("./customerController");
 
 /**
  * Creates a new order in the Orders collection.
@@ -8,50 +10,56 @@ var moment = require('moment') // JS time module (run npm install moment in this
  * throughout this function, it uses the 'checkIfOrderExists' helper.
  */
 const newOrder = async (req, res, next) => {
-  const _chosenBarber = req.body.barberId 
-  const _selectedDate = req.body.orderDate
-  const _hour = req.body.orderHour
+  const _chosenBarber = req.body.barberId;
+  const _selectedDate = req.body.orderDate;
+  const _hour = req.body.orderHour;
   try {
-      if (await checkIfOrderExists(_chosenBarber, _selectedDate, _hour)) {
-        alert("You have already made this appointments");
-        res.send(false)
-      } else {
-        await db.collection("Orders").add({
-            barberId: _chosenBarber,
-            customerId: req.body.customerId,
-            orderDate: _selectedDate,
-            orderHour: _hour,
-            extra_info: "",
-            customerName: req.body.customerName,
-            barberName: req.body.barberName
-            })
-            .then(() => {
-              res.send(true);
-            });
-      }
-  }catch (e) {
-    console.log(`Error adding document: ${e}`)
+    if (await checkIfOrderExists(_chosenBarber, _selectedDate, _hour)) {
+      alert("You have already made this appointments");
+      res.send(false);
+    } else {
+      await db
+        .collection("Orders")
+        .add({
+          barberId: _chosenBarber,
+          customerId: req.body.customerId,
+          orderDate: _selectedDate,
+          orderHour: _hour,
+          extra_info: "",
+          customerName: req.body.customerName,
+          barberName: req.body.barberName,
+        })
+        .then((docSnapshot) => {
+          res.send(JSON.stringify(docSnapshot.id));
+        });
+    }
+  } catch (e) {
+    console.log(`Error adding document: ${e}`);
     res.send(false);
   }
 };
 
 const getBarberOrders = async (req, res, next) => {
-    const uid = req.params.id;
-    let orders = {};
-    await db.collection("Orders").get().then((querySnapshot) => {
+  const uid = req.params.id;
+  let orders = {};
+  await db
+    .collection("Orders")
+    .get()
+    .then((querySnapshot) => {
       querySnapshot.forEach((documentSnapshot) => {
-        const isActive = documentSnapshot.data().orderDate >= moment(new Date()).format("YYYY-MM-DD");
-        const isUsersOrder = documentSnapshot.data().barberId === uid;        
+        const isActive =
+          documentSnapshot.data().orderDate >=
+          moment(new Date()).format("YYYY-MM-DD");
+        const isUsersOrder = documentSnapshot.data().barberId === uid;
         if (isActive && isUsersOrder) {
           orders[documentSnapshot.id] = documentSnapshot.data();
         }
       });
-      })
-      .catch((err) => {
-        res.status(400).send(err.message);
-      });
-    console.log(orders)
-    res.send(orders); 
+    })
+    .catch((err) => {
+      res.status(400).send(err.message);
+    });
+  res.send(orders);
 };
 
 /**
@@ -62,37 +70,45 @@ const getBarberOrders = async (req, res, next) => {
  */
 const getCustomerOrders = async (req, res, next) => {
   const uid = req.params.id;
-    let orders = {};
-    await db.collection("Orders").get().then((querySnapshot) => {
+  let orders = {};
+  await db
+    .collection("Orders")
+    .get()
+    .then((querySnapshot) => {
       querySnapshot.forEach((documentSnapshot) => {
-        const isActive = documentSnapshot.data().orderDate >= moment(new Date()).format("YYYY-MM-DD");
-        const isUsersOrder = documentSnapshot.data().customerId === uid;      
+        const isActive =
+          documentSnapshot.data().orderDate >=
+          moment(new Date()).format("YYYY-MM-DD");
+        const isUsersOrder = documentSnapshot.data().customerId === uid;
         if (isActive && isUsersOrder) {
           orders[documentSnapshot.id] = documentSnapshot.data();
         }
       });
-      })
-      .catch((err) => {
-        res.status(400).send(err.message);
-      });
-    console.log(orders);
-    res.send(orders); 
+    })
+    .catch((err) => {
+      res.status(400).send(err.message);
+    });
+  res.send(orders);
 };
 
 /**
- * function for returning the 
- * @param {*} uid 
- * @returns 
+ * function for returning the
+ * @param {*} uid
+ * @returns
  */
 const getBarber = async (uid) => {
-  const userData = await db.collection("Barbers").doc(uid).get().catch((err) => {
-    throw Error(err);
-  });
+  const userData = await db
+    .collection("Barbers")
+    .doc(uid)
+    .get()
+    .catch((err) => {
+      throw Error(err);
+    });
   return userData.data();
 };
 
 /**
- * function for getting the available appointments -> so you wont set two appointments on the same day. 
+ * function for getting the available appointments -> so you wont set two appointments on the same day.
  * It's using the getBarber helper, which returns the barbers details.
  */
 const getAvailableAppointments = async (req, res, next) => {
@@ -132,7 +148,7 @@ const getAvailableAppointments = async (req, res, next) => {
 };
 
 /**
- * Function for connecting the DB, and delete an order by the order ID. 
+ * Function for connecting the DB, and delete an order by the order ID.
  * Post function, Getting back the error code in case of failure, 'Order Deleted!' as DR in case of success.
  */
 const deleteOrder = async (req, res, next) => {
@@ -141,6 +157,7 @@ const deleteOrder = async (req, res, next) => {
   const time = req.body.time;
   const key = req.body.key;
   if (barberId && date && time && key) {
+    await deleteFromWaitlist(key);
     await db
       .collection("Orders")
       .doc(key)
@@ -148,20 +165,27 @@ const deleteOrder = async (req, res, next) => {
       .then(() => {
         res.send("Order deleted!");
       })
-      .catch((err) => res.send("Delete Order not finished!, error:",err));
+      .catch((err) => res.send("Delete Order not finished!, error:", err));
   }
 };
 
+
+const deleteFromWaitlist = async (orderId) => {
+  await db.collection("WaitList").doc(orderId).delete().catch(err=>{console.log(err.message)});
+}
 /**
  * This is a inner helper function for 'newOrder' function. it's goes to the DB and search if an order is exists by:
  * @param {*} _chosenBarber - the barber ID's as he appears on Firestore.
  * @param {*} _selectedDate - the date selected on 'YYYY-MM-DD' format.
  * @param {*} _hour - order hour, by 'mm:ss' format.
- * @returns true if it already exists, false otherwise. 
+ * @returns true if it already exists, false otherwise.
  */
 const checkIfOrderExists = async (_chosenBarber, _selectedDate, _hour) => {
   let exists = false;
-  await db.collection("Orders").get().then((querySnapshot) => {
+  await db
+    .collection("Orders")
+    .get()
+    .then((querySnapshot) => {
       querySnapshot.forEach((documentSnapshot) => {
         if (
           documentSnapshot.data().Barber_id === _chosenBarber &&
@@ -174,19 +198,100 @@ const checkIfOrderExists = async (_chosenBarber, _selectedDate, _hour) => {
       });
     })
     .catch((err) => {
-      console.log(`EROR: ${err}`)
+      console.log(`EROR: ${err}`);
       alert(err);
     });
-    return exists
-
+  return exists;
 };
 
+const addOrderToWaitlist = async (req, res, next) => {
+  await db
+    .collection("WaitList")
+    .doc(req.body.orderId)
+    .add({ orderId: req.body.orderId })
+    .catch((err) => {
+      res.status(400).send(err.message);
+    });
+  res.send("Success");
+};
+
+const getOrder = async (orderId) => {
+  const order = await db.collection("Orders").doc(orderId).get();
+  return order.data();
+};
+
+const findWaitlistOrderOnDelete = async (req, res) => {
+  console.log("here 218");
+  const newOrderId = req.params.id;
+  const order = await getOrder(newOrderId);
+  console.log(order);
+  const date = order.orderDate;
+  const time = order.orderHour;
+  const today = moment(new Date()).format("YYYY-MM-DD");
+  const now = moment().format("HH:mm");
+  await db
+    .collection("WaitList")
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach(async (documentSnapshot) => {
+        const docOrder = await getOrder(documentSnapshot.data().orderId);
+        if (docOrder.barberId === order.barberId) {
+          const waitListCandidate = docOrder;
+          const orderDate = waitListCandidate.orderDate;
+          const orderTime = waitListCandidate.orderHour;
+          const customer = await getCustomerFromDB(
+            waitListCandidate.customerId
+          );
+          const name = customer.userName;
+          const email = customer.userEmail;
+          if (orderDate <= today && orderTime <= now) {
+            documentSnapshot.delete();
+          } else if (date <= orderDate && time < orderTime) {
+            await sendMailToWaitlistCustomer(
+              documentSnapshot.data().orderId,
+              newOrderId,
+              email,
+              name,
+              date,
+              time
+            );
+          }
+        }
+      });
+      res.send("Success!");
+    })
+    .catch((err) => {
+      res.status(400).send(err.message);
+    });
+};
+
+const updateAppointment = async (req, res) => {
+  const oldOrderId = req.params.oldOrder;
+  const newDate = req.params.date;
+  const newTime = req.params.time;
+  await db
+    .collection("Orders")
+    .doc(oldOrderId)
+    .update({
+      orderDate: newDate,
+      orderHour: newTime,
+    })
+    .catch((err) => {
+      res.status(400).send(err.message);
+    });
+  await deleteFromWaitlist(oldOrderId);
+  res.send("You have successfully updated your appointment!");
+};
 
 module.exports = {
-    newOrder,
-    getBarberOrders,
-    getCustomerOrders,
-    getAvailableAppointments,
-    deleteOrder,
-    checkIfOrderExists
-}
+  newOrder,
+  getBarberOrders,
+  getCustomerOrders,
+  getAvailableAppointments,
+  deleteOrder,
+  checkIfOrderExists,
+  addOrderToWaitlist,
+  getOrder,
+  findWaitlistOrderOnDelete,
+  updateAppointment,
+};
--- routes/order-routes.js
+++ routes/order-routes.js
@@ -4,8 +4,13 @@ const {
         getBarberOrders,
         getCustomerOrders,
         getAvailableAppointments,
-        deleteOrder
+        deleteOrder,
+        addOrderToWaitlist,
+        getOrder,
+        findWaitlistOrderOnDelete,
+        updateAppointment
 } =  require('../controllers/orderController');
+const {sendMail} = require('../controllers/mailController');
 
 const router = express.Router();
 
@@ -16,7 +21,11 @@ router.get('/orderByBarber/:id',getBarberOrders);
 router.get('/orderCustomer/:id',getCustomerOrders);
 router.get('/availableAppointments/:id/:date',getAvailableAppointments);
 router.post('/deleteOrder', deleteOrder)
-
+router.post('/waitlist', addOrderToWaitlist)
+router.post('/sendmail',sendMail);
+router.get('/order/:id',getOrder);
+router.post('/findearlyorder/:id',findWaitlistOrderOnDelete);
+router.get('/updateappointment/:oldOrder/:date/:time',updateAppointment);
 
 module.exports = {
     routes: router
